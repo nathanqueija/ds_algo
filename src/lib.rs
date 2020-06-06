48fd2ce34427a3ac0d1420d82721ef754c89d387
Nathan <nathanqcam@gmail.com>
2020-06-06 14:17:28 +0200

    Fix repo structure

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! A library that I've developer while I was learning Rust.
+//! Here you''l find the most common algorithms and also studies about runtime complexities
+//!
+//! # Algorithms
+//! - Sorting
+//!
+//!
+
+pub mod algorithms;
